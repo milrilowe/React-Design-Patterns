4bf65458ab967abfa748a22132347bd08b7d1379
Miles Owens <milesrhowens@gmail.com>
2023-06-20 10:20:44 -0700

    Remove unused function from LoginPage index.js

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -2,10 +2,4 @@ import React from "react";
 import LoginPage from "./LoginPage";
 import useLayout from "../../hooks/useLayout";
 
-function LoginPageContainer() {
-  const LoginPageWithSelectedLayout = useLayout(LoginPage);
-
-  return <LoginPageWithSelectedLayout />;
-}
-
 export default () => useLayout(LoginPage);
