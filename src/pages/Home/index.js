3bef452c286853f45421485a8d81400b08c0d98a
Miles Owens <milesrhowens@gmail.com>
2023-06-20 10:21:01 -0700

    Remove unused function from HomePage index.js

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,10 +2,4 @@ import React from "react";
 import HomePage from "./HomePage";
 import useLayout from "../../hooks/useLayout";
 
-function HomePageContainer() {
-  const HomePageWithSelectedLayout = useLayout(HomePage);
-
-  return <HomePageWithSelectedLayout />;
-}
-
 export default () => useLayout(HomePage);
